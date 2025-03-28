09026cee76f1620801d26c86f9763f7dfb1905d0
giorgi.merabishvili <g.merabishvili@rs.ge>
2025-03-28 11:56:36 +0400

    add-hotels-initialmigration

---
+++ Booking_API_CleanArchitecture/Migrations/20250328075318_initialWithHotelsAndAdmin.cs
@@ -0,0 +1,264 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+#pragma warning disable CA1814 // Prefer jagged arrays over multidimensional
+
+namespace Booking_API_CleanArchitecture.Migrations
+{
+    /// <inheritdoc />
+    public partial class initialWithHotelsAndAdmin : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "ExceptionLogs",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    StatusCode = table.Column<int>(type: "int", nullable: false),
+                    Message = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    StackTrace = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    CreateDate = table.Column<DateTime>(type: "datetime2", nullable: false),
+                    LastModifiedDate = table.Column<DateTime>(type: "datetime2", nullable: true),
+                    CreatorId = table.Column<int>(type: "int", nullable: true),
+                    ModifierId = table.Column<int>(type: "int", nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_ExceptionLogs", x => x.Id);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "Hotels",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    Name = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    Address = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    City = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    hotelImage = table.Column<string>(type: "nvarchar(max)", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Hotels", x => x.Id);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "RoomTypes",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    TypeName = table.Column<string>(type: "nvarchar(max)", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_RoomTypes", x => x.Id);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "Users",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    UserName = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    Email = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    Role = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    PasswordHash = table.Column<byte[]>(type: "varbinary(max)", nullable: false),
+                    PasswordSalt = table.Column<byte[]>(type: "varbinary(max)", nullable: false),
+                    RefreshToken = table.Column<string>(type: "nvarchar(max)", nullable: true),
+                    RefreshTokenExpirationDate = table.Column<DateTime>(type: "datetime2", nullable: true),
+                    CreateDate = table.Column<DateTime>(type: "datetime2", nullable: false),
+                    LastModifiedDate = table.Column<DateTime>(type: "datetime2", nullable: true),
+                    CreatorId = table.Column<int>(type: "int", nullable: true),
+                    ModifierId = table.Column<int>(type: "int", nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Users", x => x.Id);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "Rooms",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    Name = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    HotelId = table.Column<int>(type: "int", nullable: false),
+                    PricePerNight = table.Column<decimal>(type: "decimal(18,2)", nullable: false),
+                    Available = table.Column<bool>(type: "bit", nullable: false),
+                    MaximumGuests = table.Column<int>(type: "int", nullable: false),
+                    RoomTypeId = table.Column<int>(type: "int", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Rooms", x => x.Id);
+                    table.ForeignKey(
+                        name: "FK_Rooms_Hotels_HotelId",
+                        column: x => x.HotelId,
+                        principalTable: "Hotels",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Cascade);
+                    table.ForeignKey(
+                        name: "FK_Rooms_RoomTypes_RoomTypeId",
+                        column: x => x.RoomTypeId,
+                        principalTable: "RoomTypes",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Cascade);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "BookedDates",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    RoomId = table.Column<int>(type: "int", nullable: false),
+                    StartDate = table.Column<DateTime>(type: "datetime2", nullable: false),
+                    EndDate = table.Column<DateTime>(type: "datetime2", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_BookedDates", x => x.Id);
+                    table.ForeignKey(
+                        name: "FK_BookedDates_Rooms_RoomId",
+                        column: x => x.RoomId,
+                        principalTable: "Rooms",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Cascade);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "Bookings",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    RoomId = table.Column<int>(type: "int", nullable: false),
+                    CheckInDate = table.Column<DateTime>(type: "datetime2", nullable: false),
+                    CheckOutDate = table.Column<DateTime>(type: "datetime2", nullable: false),
+                    TotalPrice = table.Column<decimal>(type: "decimal(18,2)", nullable: false),
+                    IsConfirmed = table.Column<bool>(type: "bit", nullable: false),
+                    CustomerName = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    CustomerId = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    CustomerPhone = table.Column<string>(type: "nvarchar(max)", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Bookings", x => x.Id);
+                    table.ForeignKey(
+                        name: "FK_Bookings_Rooms_RoomId",
+                        column: x => x.RoomId,
+                        principalTable: "Rooms",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Cascade);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "Images",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    RoomId = table.Column<int>(type: "int", nullable: false),
+                    roomImage = table.Column<string>(type: "nvarchar(max)", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Images", x => x.Id);
+                    table.ForeignKey(
+                        name: "FK_Images_Rooms_RoomId",
+                        column: x => x.RoomId,
+                        principalTable: "Rooms",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Cascade);
+                });
+
+            migrationBuilder.InsertData(
+                table: "Hotels",
+                columns: new[] { "Id", "Address", "City", "Name", "hotelImage" },
+                values: new object[,]
+                {
+                    { 1, "123 Main Street, City Center", "Tbilisi", "Chembers Grand Hotel", "/hotel_images/tbilisi-chembers.JPG" },
+                    { 2, "456 River Road, Scenic Area", "Kutaisi", "Episode Retreat", "/hotel_images/episode.JPG" }
+                });
+
+            migrationBuilder.InsertData(
+                table: "RoomTypes",
+                columns: new[] { "Id", "TypeName" },
+                values: new object[,]
+                {
+                    { 1, "Single Room" },
+                    { 2, "Double Room" },
+                    { 3, "Triple Room" },
+                    { 4, "Deluxe Room" },
+                    { 5, "Family Room" }
+                });
+
+            migrationBuilder.InsertData(
+                table: "Users",
+                columns: new[] { "Id", "CreateDate", "CreatorId", "Email", "LastModifiedDate", "ModifierId", "PasswordHash", "PasswordSalt", "RefreshToken", "RefreshTokenExpirationDate", "Role", "UserName" },
+                values: new object[] { 1, new DateTime(2025, 3, 28, 11, 53, 17, 729, DateTimeKind.Local).AddTicks(3675), null, "admin@email.com", null, null, new byte[] { 21, 195, 192, 15, 109, 10, 37, 66, 111, 6, 236, 85, 69, 215, 41, 215, 8, 104, 86, 75, 218, 158, 84, 112, 85, 211, 167, 124, 155, 240, 238, 157, 197, 45, 40, 10, 234, 120, 135, 23, 123, 130, 164, 5, 208, 155, 0, 242, 242, 93, 36, 211, 237, 183, 22, 5, 74, 22, 59, 254, 74, 40, 14, 7 }, new byte[] { 114, 47, 106, 104, 52, 183, 98, 64, 110, 20, 93, 100, 22, 75, 114, 9, 135, 173, 126, 175, 103, 225, 254, 246, 126, 48, 171, 65, 46, 95, 74, 223, 211, 185, 179, 236, 201, 168, 154, 188, 156, 21, 89, 54, 43, 149, 94, 86, 119, 92, 234, 196, 134, 149, 229, 211, 102, 5, 230, 143, 185, 132, 41, 169, 20, 141, 118, 115, 59, 75, 16, 150, 10, 1, 238, 145, 200, 239, 112, 126, 183, 87, 70, 6, 144, 136, 218, 184, 160, 215, 95, 74, 73, 117, 220, 115, 105, 69, 53, 45, 126, 55, 88, 114, 211, 223, 191, 220, 132, 68, 243, 86, 190, 55, 239, 87, 103, 211, 202, 40, 1, 216, 80, 240, 207, 98, 1, 7 }, null, null, "Admin", "admin" });
+
+            migrationBuilder.CreateIndex(
+                name: "IX_BookedDates_RoomId",
+                table: "BookedDates",
+                column: "RoomId");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Bookings_RoomId",
+                table: "Bookings",
+                column: "RoomId");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Images_RoomId",
+                table: "Images",
+                column: "RoomId");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Rooms_HotelId",
+                table: "Rooms",
+                column: "HotelId");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Rooms_RoomTypeId",
+                table: "Rooms",
+                column: "RoomTypeId");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "BookedDates");
+
+            migrationBuilder.DropTable(
+                name: "Bookings");
+
+            migrationBuilder.DropTable(
+                name: "ExceptionLogs");
+
+            migrationBuilder.DropTable(
+                name: "Images");
+
+            migrationBuilder.DropTable(
+                name: "Users");
+
+            migrationBuilder.DropTable(
+                name: "Rooms");
+
+            migrationBuilder.DropTable(
+                name: "Hotels");
+
+            migrationBuilder.DropTable(
+                name: "RoomTypes");
+        }
+    }
+}
--- Booking_Infrastructure/Persistence/ApplicationDbContext.cs
+++ Booking_Infrastructure/Persistence/ApplicationDbContext.cs
@@ -1,7 +1,6 @@
 ﻿using Booking_Domain.Entities;
 using Booking_Domain.Models;
 using Microsoft.EntityFrameworkCore;
-
 namespace Booking_Infrastructure.Persistence // Change to the correct namespace
 {
     public class ApplicationDbContext : DbContext
@@ -20,6 +19,31 @@ namespace Booking_Infrastructure.Persistence // Change to the correct namespace
 
         protected override void OnModelCreating(ModelBuilder modelBuilder)
         {
+            static (byte[] passwordHash, byte[] passwordSalt) CreatePasswordHash(string password)
+            {
+                using (var hmac = new System.Security.Cryptography.HMACSHA512())
+                {
+                    var passwordSalt = hmac.Key;
+                    var passwordHash = hmac.ComputeHash(System.Text.Encoding.UTF8.GetBytes(password));
+                    return (passwordHash, passwordSalt);
+                }
+            }
+
+
+            var (passwordHash, passwordSalt) = CreatePasswordHash("Admin!234");
+            modelBuilder.Entity<User>().HasData(
+                 new User
+                 {
+                  Id = 1,
+                  UserName = "admin",
+                  Email = "admin@email.com",
+                  Role = "Admin",
+                  PasswordHash = passwordHash,
+                  PasswordSalt = passwordSalt
+                  }
+             );
+
+
             // Ensure Id is the primary key for each model
             modelBuilder.Entity<Room>().HasKey(r => r.Id);
             modelBuilder.Entity<BookedDate>().HasKey(b => b.Id);
@@ -32,6 +56,47 @@ namespace Booking_Infrastructure.Persistence // Change to the correct namespace
                 new RoomType { Id = 4, TypeName = "Deluxe Room" },
                 new RoomType { Id = 5, TypeName = "Family Room" }
             );
+
+
+            //Add Hotels On Initial
+            static string GenerateSampleImagePath(string hotelName)
+            {
+                var uploadsFolder = Path.Combine(Directory.GetCurrentDirectory(), "wwwroot/hotel_images");
+                Directory.CreateDirectory(uploadsFolder);
+
+                // Use a consistent filename based on the hotel name
+                string fileExtension = ".jpg";
+                string uniqueFileName = $"{hotelName.Replace(" ", "_").ToLower()}{fileExtension}";
+                string filePath = Path.Combine(uploadsFolder, uniqueFileName);
+
+                // Return the relative path for database storage
+                return $"/hotel_images/{uniqueFileName}";
+            }
+
+            // Seed Hotels
+            modelBuilder.Entity<Hotel>().HasData(
+                new Hotel
+                {
+                    Id = 1,
+                    Name = "Chembers Grand Hotel",
+                    Address = "123 Main Street, City Center",
+                    City = "Tbilisi",
+                    hotelImage = "/hotel_images/tbilisi-chembers.JPG"
+                },
+                new Hotel
+                {
+                    Id = 2,
+                    Name = "Episode Retreat",
+                    Address = "456 River Road, Scenic Area",
+                    City = "Kutaisi",
+                    hotelImage = "/hotel_images/episode.JPG"
+                }
+            );
+
+
+
+            base.OnModelCreating(modelBuilder);
+
         }
     }
 }
